45a60874ee8ee4de9856dd0e2155d99c6b50e286
Critter & Guitari <critter@critterandguitari.com>
2017-01-16 12:43:03 +0000

    update shutdown script

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 oscsend localhost 4001 /shutdown i 1
-/root/scripts/killpd.sh
-/root/scripts/killmother.sh
 killall create_ap
 killall wpa_supplicant
+sleep .1
 shutdown -h now
-#echo "shjutting down"
